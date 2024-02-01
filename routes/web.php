67ee507e07ba9a7b8e5b0313076a4d4883a2dbb6
TOM <tom.ribenceira@outlook.com>
2024-02-01 15:38:50 +0100

    m

--- routes/web.php
+++ routes/web.php
@@ -3,18 +3,19 @@
 use Illuminate\Support\Facades\Route;
 
 
+
 Route::get('/', function () {
-    return view('layouts/home');
+    return view('home');
 });
 
-// Route::get('/destination', function () {
-//     return view('destination');
-// });
+Route::get('/destination', function () {
+    return view('destination');
+});
 
-// Route::get('/crew', function () {
-//     return view('crew');
-// });
+Route::get('/crew', function () {
+    return view('crew');
+});
 
-// Route::get('/technology', function () {
-//     return view('technology');
-// });
+Route::get('/technology', function () {
+    return view('technology');
+});
